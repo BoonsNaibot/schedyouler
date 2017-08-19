1487738f7f4e3d24be8dc2b9be4b12bab24c008e
divine <divine@divines-iMac.local>
2017-08-19 11:46:53 -0500

    Initial Commit

---
+++ schedyouler/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  schedyouler
+//
+//  Created by divine on 8/19/17.
+//  Copyright © 2017 divine ikenna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
